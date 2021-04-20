099d968ca60878bb3cc6961d1943e5cd935e8aa2
qtpsudhakar <sudhakar@qtpsudhakar>
2021-04-20 17:31:58 +0530

    Read excel File

--- src/main/java/com/files/handle/ReadData_Excel.java
+++ src/main/java/com/files/handle/ReadData_Excel.java
@@ -1,23 +1,33 @@
 package com.files.handle;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class ReadData_Excel {
     public static void main(String[] args) {
 
         String flPath ="src\\main\\resources\\Data.xlsx";
-        File fl = new File(flPath);
+
+
         //xssf : xlsx
         //hssf : xls
 
         try {
-            Workbook wb = new XSSFWorkbook(fl);
+            FileInputStream fl = new FileInputStream(flPath);
+            Workbook wb;
+            if (flPath.endsWith(".xlsx")){
+                wb = new XSSFWorkbook(fl);
+            }else {
+                wb = new HSSFWorkbook(fl);
+            }
+
             Sheet sht = wb.getSheet("Sheet1");
             int rc = sht.getLastRowNum();
             int cc = sht.getRow(0).getLastCellNum();
@@ -33,8 +43,6 @@ public class ReadData_Excel {
             wb.close();
 
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InvalidFormatException e) {
             e.printStackTrace();
         }
     }
