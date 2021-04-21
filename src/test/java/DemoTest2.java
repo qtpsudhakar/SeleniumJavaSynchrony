73c4d08acbdd0ab3e5856e9e7af38901bad7651b
qtpsudhakar <sudhakar@qtpsudhakar>
2021-04-21 16:03:24 +0530

    Create Tests

---
+++ src/test/java/DemoTest2.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class DemoTest2 {
+
+    @Test
+    public void test3(){
+        System.out.println("this is demo test3");
+    }
+    
+    @Test
+    public void test1(){
+        System.out.println("this is demo test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("this is demo test2");
+    }
+}
